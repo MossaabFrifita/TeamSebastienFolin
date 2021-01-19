db236c8455aaa4cba076bdda14d7069aa7fa242e
MossaabFrifita <frifita1@gmail.com>
2021-01-19 16:57:26 +0100

    getting lat and long on touch in mkmapview

---
+++ TeamSebastienFolin/Service.swift
@@ -0,0 +1,14 @@
+//
+//  Service.swift
+//  TeamSebastienFolin
+//
+//  Created by etudiant on 19/01/2021.
+//  Copyright © 2021 etudiant. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class Service {
+    
+}
---
+++ TeamSebastienFolin/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  TeamSebastienFolin
+//
+//  Created by etudiant on 19/01/2021.
+//  Copyright © 2021 etudiant. All rights reserved.
+///Users/etudiant/.gem/ruby/2.6.0/bin/pod
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+ 
+        mapView.delegate = self
+        let longTapGesture = UILongPressGestureRecognizer(target: self, action: #selector(longTap))
+        mapView.addGestureRecognizer(longTapGesture)
+        
+    }
+    
+    @objc func longTap(sender: UIGestureRecognizer){
+        print("long tap")
+        if sender.state == .began {
+            let locationInView = sender.location(in: mapView)
+            let locationOnMap = mapView.convert(locationInView, toCoordinateFrom: mapView)
+            //addAnnotation(location: locationOnMap)
+            print(locationOnMap.latitude)
+            print(locationOnMap.longitude)
+            
+            
+        }
+    }
+
+}
+
+extension ViewController: MKMapViewDelegate{
+
+func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+    guard annotation is MKPointAnnotation else { print("no mkpointannotaions"); return nil }
+    
+    let reuseId = "pin"
+    var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+    
+    if pinView == nil {
+        pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+        pinView!.canShowCallout = true
+        pinView!.rightCalloutAccessoryView = UIButton(type: .infoDark)
+        pinView!.pinTintColor = UIColor.black
+    }
+    else {
+        pinView!.annotation = annotation
+    }
+    return pinView
+}
+}
+
